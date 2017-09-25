a7c9ac7467215e9740fc6d4e69248a9ee3ffb9c6
Tobias Möhring <dasbente@gmail.com>
2017-09-25 17:30:10 +0200

    Added Pug to render view templates.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// Set Pug as view engine (Template location defaults to ./views)
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-    res.send('Hello, World!');
+    res.render('home', {
+	title: 'Hello, World!'
+    });
 });
 
 app.listen(port, function () {

b52057a09c0a5eaef3ca2bb8aab038076b2d0b6e
Tobias Möhring <dasbente@gmail.com>
2017-09-27 14:29:40 +0200

    Slightly modularized index.js and added error handling

---
+++ app.js
@@ -0,0 +1,47 @@
+/**
+ * This programm sets up anything around representation and navigation of this webserver. Essentially this 
+ *  servers backbone.
+ */
+
+// The app will make use of express for all it's page-related functionallity
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const routes = require('./routes');
+
+// Set Pug as view engine 
+app.set('view engine', 'pug');
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Register node modules and public files with the express server
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setup REST routing
+app.get('/helloworld', function (req, res) {
+    res.render('home', {
+	title: 'Hello, World!'
+    });
+});
+
+// Forward any caught 404s to the error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handling
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+
+    res.render('error', {
+	message: err.message,
+	error: app.get('env') === 'development' ? err : {}
+    });
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+/**
+ * This programm concerns itself with starting up and controlling all aspects and services of the webserver.
+ */
 
-// Set Pug as view engine (Template location defaults to ./views)
-app.set('view engine', 'pug');
+// We need a http server for any non-express functionallity
+const http = require('http');
+const app = require('./app');
 
-app.get('/', function (req, res) {
-    res.render('home', {
-	title: 'Hello, World!'
-    });
+// Retrieve a port from environment to be used by this server
+const port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+// Start HTTP server and begin listening to it
+const server = http.createServer(app);
+server.listen(port);
+
+// HTTP server events
+server.on('listening', function () {
+    console.log('Listening on port '+ server.address().port);
 });
 
-app.listen(port, function () {
-    console.log('Example app listening on port '+ port +'!');
+server.on('error', function (err) {
+    if (error.syscall !== 'listen') {
+	throw err;
+    }
+
+    // Determine the type of the port used by our HTTP server
+    let bind = typeof port === 'string' ? 'Pipe '+ port : 'Port '+ port;
+
+    switch (error.code) {
+    case 'EACCES':
+	console.error(bind + ' requires elevated privileges');
+	process.exit(1);
+	break;
+
+    case 'EADDRINUSE':
+	console.error(bind + ' is already in use');
+	process.exit(1);
+	break;
+
+    default:
+	throw err;
+    }
 });
+
+/* Helper function to retrieve the port from a string delivered by the environment */
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+
+    if (isNaN(port)) { // The port is a named pipe
+	return val;
+    } else if (port >= 0) { // The port is a port number
+	return port;
+    } else {
+	return false;
+    }
+}
